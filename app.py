e01eede86678464378cfb90120d0ffdabad7b383
alanCrtl <alan.courtel1999@gmail.com>
2023-10-06 15:02:26 +0200

    woow ca marche pas

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask
-
+from flask import Flask, render_template
 import pymongo
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Connect to your MongoDB instance
+client = MongoClient("mongodb://root:pass@27017:27017/spotifydb")
+db = client['spotifydb'] # Replace with your database name
+collection = db['spot']  # Replace with your collection name
+data = collection.find({})
 
 @app.route('/')
 def main():
-    return "Hello there.."
+    res = "Hello there..<br>"
+    res += "Connected to MongoDB: " + str(client) + "<br>"
+    res += "Selected database: " + str(db) + "<br>"
+    res += "Selected collection: " + str(collection)+"<br><<"   
+    return res
+
+@app.route('/dataview')
+def view_data():
+    # Retrieve data from MongoDB collection
+    data = collection.find({})
+    
+    # Render the template with the retrieved data
+    return render_template('dataview.html', data=data)
 
 
 if __name__ == "__main__":
