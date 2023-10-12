b5465c0af72ed5400143dd0ab430b2c4e5cf96f0
magic <samsgrice@gmail.com>
2023-10-12 09:12:41 +0200

    save before changing

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from bson import json_util
 
-import pymongo
+import pymongo, json
 import pandas as pd
 from pymongo import MongoClient
 
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+
 app = Flask(__name__)
+
+
+CONNECTION_STRING = "mongodb://root:pass@localhost:27017/?authMechanism=DEFAULT"
+client = MongoClient(CONNECTION_STRING)
+db = client["Spotify"]
+attribs = db["attributes"]
 
 @app.route('/')
 def main():
-    client = connect_to_database()
-    db = client["Spotify"]
-    attribs = db["attributes"]
     count = attribs.count_documents({'danceability': {'$gt': 0.4}})
-    return f"{count}"
+    return f"c:{count}"
 
-def connect_to_database():
-    CONNECTION_STRING = "mongodb://root:pass@localhost:27017/?authMechanism=DEFAULT"
-    return MongoClient(CONNECTION_STRING)
+@app.route('/query')
+def query_mongo():
+    query = request.args.get("q")
+    try:
+        query_dict = eval(query)
+    except Exception as e:
+        return jsonify({'error', 'invalid query'}), 400
+    try:
+        print(query)
+        print(query_dict)
+        res = list(attribs.find(query_dict))
+        return parse_json(res), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 def insert_csv_data(csv_path, db, collection_name):
     collection = db[collection_name]
@@ -25,7 +44,4 @@ def insert_csv_data(csv_path, db, collection_name):
 
 
 if __name__ == "__main__":
-    client = connect_to_database()
-    db = client["Spotify"]
-    insert_csv_data("Spotify_Song_Attributes.csv", db, "attributes")
     app.run(host="0.0.0.0", port=5000)
