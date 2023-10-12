3d5e23f663b6c2d8a09cabe667ba67d8ca3d2217
magic <samsgrice@gmail.com>
2023-10-12 10:56:28 +0200

    works

--- app.py
+++ app.py
@@ -1,49 +1,28 @@
-from flask import Flask, request, jsonify
-from bson import json_util
-import pymongo, json
-import pandas as pd
+from flask import Flask, jsonify
+import pymongo
 from pymongo import MongoClient
-
-
-def insert_csv_data(csv_path, db, collection_name):
-    collection = db[collection_name]
-    data = pd.read_csv(csv_path)
-    collection.insert_many(data.to_dict(orient="records"))
-
-def parse_json(data):
-    return json.loads(json_util.dumps(data))
-
 
 app = Flask(__name__)
 
-CONNECTION_STRING = "mongodb://root:pass@localhost:27017/?authMechanism=DEFAULT"
-client = MongoClient(CONNECTION_STRING)
-db = client["Spotify"]
-print("Loading data")
-insert_csv_data("Spotify_Song_Attributes.csv", db, "attributes")
-print("Data Loaded !")
-attribs = db["attributes"]
+def get_db():
+    client = MongoClient(host='test_mongodb',
+                         port=27017,
+                         username='root',
+                         password='pass',
+                        authSource="admin")
+    db = client["animal_db"]
+    return db
 
 @app.route('/')
-def main():
-    count = attribs.count_documents({'danceability': {'$gt': 0.4}})
-    return f"c:{count}"
+def ping_server():
+    return "Welcome to the world of animals."
 
-@app.route('/query')
-def query_mongo():
-    query = request.args.get("q")
-    try:
-        query_dict = eval(query)
-    except Exception as e:
-        return jsonify({'error', 'invalid query'}), 400
-    try:
-        print(query)
-        print(query_dict)
-        res = list(attribs.find(query_dict))
-        return parse_json(res), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+@app.route('/animals')
+def get_stored_animals():
+    db = get_db()
+    _animals = db.animal_tb.find()
+    animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
+    return jsonify({"animals": animals})
 
-
-if __name__ == "__main__":
+if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
