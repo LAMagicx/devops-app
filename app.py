c58bada1c183941bde04c14df5ee22f355637839
magic <samsgrice@gmail.com>
2023-10-12 16:30:11 +0200

    deving

--- app.py
+++ app.py
@@ -1,28 +1,101 @@
 from flask import Flask, jsonify
 import pymongo
+from flask import Flask, render_template_string, request, jsonify
+import plotly.graph_objects as go
+import plotly.io as pio
+import pandas as pd
+import numpy as np
+import plotly.express as px
 from pymongo import MongoClient
+from template import HISTO, BARGRAPH, RAWDATA
+from sklearn.decomposition import PCA
 
-app = Flask(__name__)
+def insert_csv_data(csv_path, db, collection_name):
+    collection = db[collection_name]
+    data = pd.read_csv(csv_path)
+    collection.insert_many(data.to_dict(orient="records"))
 
-def get_db():
-    client = MongoClient(host='test_mongodb',
-                         port=27017,
-                         username='root',
-                         password='pass',
-                        authSource="admin")
-    db = client["animal_db"]
-    return db
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
 
-@app.route('/')
-def ping_server():
-    return "Welcome to the world of animals."
+def calculate_song_distance(s1, s2):
+    numerical_features = ["danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "valence", "tempo"]
 
-@app.route('/animals')
-def get_stored_animals():
-    db = get_db()
-    _animals = db.animal_tb.find()
-    animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
-    return jsonify({"animals": animals})
+    return np.linalg.norm(s1[numerical_features].values, s2[numerical_features].values)
 
-if __name__=='__main__':
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/genre-bar-graph')
+def genre_bar_graph():
+    # Query data from the MongoDB collection and group by genre
+    pipeline = [
+        {"$match": {"genre": {"$ne": "no value"}}},
+        {"$group": {"_id": "$genre", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}
+    ]
+    cursor = collection.aggregate(pipeline)
+    data = list(cursor)
+    # Extract the genre and count information
+    genres = [entry['_id'] for entry in data]
+    counts = [entry['count'] for entry in data]
+    # Create a bar graph using Plotly
+    fig = go.Figure(data=[go.Bar(x=genres, y=counts)])
+    fig.update_layout(title="Number of Songs by Genre", xaxis_title="Genre", yaxis_title="Count")
+
+    # Convert the Plotly graph to HTML for rendering
+    genre_bar_graph_html = pio.to_html(fig)
+
+    return render_template_string(BARGRAPH, genre_bar_graph=genre_bar_graph_html)
+
+@app.route('/query')
+def query_mongo():
+    query = request.args.get("q")
+
+    # check if string is valid
+    try:
+        json.loads(query)
+    except json.JSONDecodeError:
+        return jsonify({'error', 'invalid query request'}), 400
+
+    res = list(collection.find(json.loads(query)))
+    return parse_json(res)
+
+@app.route('/histograms')
+def histograms():
+    # Query data from the MongoDB collection
+    cursor = collection.find({}, {"_id": 0, "danceability": 1, "energy": 1, "valence": 1})
+    data = list(cursor)
+
+    # Create a DataFrame from the MongoDB data
+    df = pd.DataFrame(data)
+
+    # Create histograms using Plotly
+    danceability_histogram = px.histogram(df, x="danceability", title="Danceability Histogram")
+    energy_histogram = px.histogram(df, x="energy", title="Energy Histogram")
+    valence_histogram = px.histogram(df, x="valence", title="Valence Histogram")
+
+    # Convert the Plotly plots to HTML for rendering
+    danceability_html = danceability_histogram.to_html()
+    energy_html = energy_histogram.to_html()
+    valence_html = valence_histogram.to_html()
+
+    return render_template_string(HISTO, danceability=danceability_html, energy=energy_html, valence=valence_html)
+
+@app.route('/analyse')
+def analyse():
+    # pca analysis of songs
+    df = pd.DataFrame(list(collection.find())).dropna()
+    numerical_features = ["danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "valence", "tempo"]
+
+    pca = PCA(n_components=2)
+    data = pca.fit_transform(df[numerical_features])
+
+    df["X"] = data[:, 0]
+    df["Y"] = data[:, 1]
+
+
+
+
+
+    return ""
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
